31a7b53afc3d5eac6ea8c2b34e3e4f94fe2be4f0
Chris <christopherrebollar0@icloud.com>
2024-07-13 22:03:19 -0700

    working on home

--- JMGWorkScape/HomeScreen.swift
+++ JMGWorkScape/HomeScreen.swift
@@ -12,41 +12,47 @@ struct HomeScreen: View {
     let olive = Color(red: 0.23, green: 0.28, blue: 0.20, opacity: 1.00)
     @State var searchText = ""
     var body: some View {
-        VStack {
-            HStack {
-                Text("Homes")
-                    .bold()
-                    .font(.largeTitle)
+        ZStack{
+            VStack {
+                HStack {
+                    Text("Homes")
+                        .bold()
+                        .font(.largeTitle)
+                        .frame(maxWidth: 350, alignment: .topLeading)
+                        .shadow(color: Color(red: 0.00, green: 0.00, blue: 0.00, opacity: 0.25), radius: 4, x: 0, y: 4)
+                        .foregroundColor(olive)
+                    
+                    Text("Label").bold()
+                    
+                }
+                TextField("'Calle Miramar..'", text: $searchText)
                     .frame(maxWidth: 350, alignment: .topLeading)
-                    .shadow(color: Color(red: 0.00, green: 0.00, blue: 0.00, opacity: 0.25), radius: 4, x: 0, y: 4)
-                    .foregroundColor(olive)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Spacer()
+                HStack{
+                    Spacer()
+                    Button {
+                    } label: {
+                        Image(systemName: "house.fill")
+                    }   .foregroundColor(olive)
+                        .font(.largeTitle)
+                    Spacer()
+                    Button {
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }   .foregroundColor(olive)
+                        .font(.largeTitle)
+                    Spacer()
+        //            Image("olive_tree 1")
+        //                .position(x: 250, y: 590)
+        //            Image("olive_grass 1")
+        //                .position(x: 190, y: 275)
+                }
+
                 
             }
-            TextField("'Calle Miramar..'", text: $searchText)
-                .frame(maxWidth: 350, alignment: .topLeading)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-
-            Button {
-                
-            } label: {
-                Image(systemName: "house.fill")
-            }   .foregroundColor(olive)
-                .font(.largeTitle)
-                .position(x: 50, y: 590)
-            
-            Button {
-                
-            } label: {
-                Image(systemName: "plus.circle.fill")
-            }   .foregroundColor(olive)
-                .font(.largeTitle)
-                .position(CGPoint(x: 50, y: 200))
-            
-            Image("olive_tree 1")
-                .position(x: 250, y: 590)
-            Image("olive_grass 1")
-                .position(x: 190, y: 275)
+            .padding()
         }
-        .padding()
+
     }
 }
