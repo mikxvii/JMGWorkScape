c99aed63e1c7ae6f355c98c9c55d28c9fdb5bda8
Michael Guerrero <47826091+mikxvii@users.noreply.github.com>
2024-07-13 19:29:21 -0700

    started on homescreen layout

--- JMGWorkScape/ContentView.swift
+++ JMGWorkScape/ContentView.swift
@@ -7,20 +7,6 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("This is crated by Chris")
-            Text("This was written by Mike")
-        }
-        .padding()
-    }
-}
-
 #Preview {
-    ContentView()
+    HomeScreen()
 }
