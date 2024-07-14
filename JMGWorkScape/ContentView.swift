a4c60c1f71edb2beb937be43ccfd248c152e5f9e
Michael Guerrero <47826091+mikxvii@users.noreply.github.com>
2024-07-13 17:42:44 -0700

    Added Swift Files

---
+++ JMGWorkScape/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JMGWorkScape
+//
+//  Created by Michael Guerrero on 7/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JMGWorkScape/JMGWorkScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JMGWorkScapeApp.swift
+//  JMGWorkScape
+//
+//  Created by Michael Guerrero on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct JMGWorkScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
